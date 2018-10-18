7e4965f99beaadfcb8b2a5b9a159b341cd742e92
KHan <han424@purdue.edu>
2018-10-18 11:42:28 -0400

    Update main_imagenet.py

--- main_imagenet.py
+++ main_imagenet.py
@@ -65,8 +65,8 @@ def main():
                                 world_size=args.world_size, rank=args.dist_rank)
 
     # Create model
-    model_name = 'PredNetResE'
-    models = {'PredNetResE':PredNetResE}
+    model_name = 'PredNetBpE'
+    models = {'PredNetBpE':PredNetBpE}
     modelname = model_name+'_'+str(args.circles)+'CLS'
     print("=> creating model '{}'".format(modelname))
     model = models[model_name](num_classes=1000,cls=args.circles)
